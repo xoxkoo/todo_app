06f0b0493fe1a643539ca59833d015798be575f8
xoxkoo <anton.durcak22@gmail.com>
2020-12-03 10:13:59 +0100

    css and javascript changes, woriking on category

--- public/src/app.js
+++ public/src/app.js
@@ -2,9 +2,11 @@
 
 import Todo from './Todo.js'
 
-const form    = document.querySelector('form'),
-      input   = document.querySelector('input[name=todo]'),
-      list    = document.querySelector('ul')
+const form         = document.querySelector('form'),
+      input        = document.querySelector('input[name=todo]'),
+      list         = document.querySelector('ul'),
+      categoryList = document.querySelector('.list'),
+      rectangle    = document.querySelector('.rectangle')
 
 const todo = new Todo(list, input)
 
@@ -12,8 +14,8 @@ form.addEventListener('submit', (e) => {
   e.preventDefault()
 
   todo.addNew()
+  todo.addClass(document.querySelector('.form-group'), 'blink-an', 500)
   todo.addClass(input, 'blink-an', 500)
-
 
 })
 
@@ -32,9 +34,31 @@ list.addEventListener('click', (e) => {
 
 })
 
-// document.onbeforeunload(() => {
-  // })
-// todo.sendData()
-
 todo.getItems()
 
+document.querySelector('body').addEventListener('click', (e) => {
+  if (e.target.classList.contains('list-form-item') && categoryList.style.display != 'block') {
+    categoryList.style.display = 'block'
+  }
+  else {
+    categoryList.style.display = 'none'
+  }
+})
+
+// adding tasks category
+document.querySelectorAll('.list-item').forEach(item => {
+
+  item.addEventListener('click', (e) => {
+    let category = e.target.textContent
+
+    document.querySelector('.list-form-item-name').textContent = category
+
+    category = category.trim() 
+
+    rectangle.classList = ''
+
+    rectangle.classList = 'rectangle list-form-item rectangle-' + category.toLowerCase()
+
+  })
+
+})
